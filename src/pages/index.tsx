cf1d11a7e1a089f75fc228738c356ba5aa35994e
Robson <robccorreia@gmail.com>
2024-03-14 23:31:49 -0300

    feat: add prettier config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import Head from "next/head";
-import styles from "../../styles/Home.module.css";
+import Head from 'next/head'
+import styles from '../../styles/Home.module.css'
 
 type Props = {
-  title: string;
-};
+  title: string
+}
 
-export default function Home({ title = "React Avançado!" }: Props) {
+export default function Home({ title = 'React Avançado!' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -19,7 +19,7 @@ export default function Home({ title = "React Avançado!" }: Props) {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
+          Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -60,10 +60,10 @@ export default function Home({ title = "React Avançado!" }: Props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  );
+  )
 }
